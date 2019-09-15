69a36c0f6461cc21d71d664c38f756e350055683
tashpool <gittash@me.com>
2019-09-14 22:11:24 -0700

    Updated Login card with Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-          text
-          href="https://github.com/vuetifyjs/vuetify/releases/latest"
-          target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+              prepend-icon="mdi-account-circle"
+              label="Username"
+          />
+          <v-text-field
+              :type="showPassword ? 'text' : 'password'"
+              label="Password"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+          />
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Login</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-app>
 </template>
 
@@ -30,7 +38,7 @@ export default {
     HelloWorld,
   },
   data: () => ({
-    //
+    showPassword: false
   }),
 };
 </script>
